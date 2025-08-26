d7a9f2c1c9b70559d758acc3200d1cff455bbc1b
heismussa <sms.mussasaid@gmail.com>
2025-08-26 19:45:11 +0300

    Added comment model

--- blog/models.py
+++ blog/models.py
@@ -15,3 +15,15 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
+class Comment(models.Model):
+    post = models.ForeignKey('Post', on_delete=models.CASCADE, related_name='comments')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=True)  # Simple moderation option
+
+    class Meta:
+        ordering = ['created_at']
+
+    def __str__(self):
+        return f"Comment by {self.user.username} on {self.post.title}"
